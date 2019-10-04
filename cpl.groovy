4940fa51a67946c2ee9061496c3024a2a73ec09b
jayabalandevops <jayabalandevops@gmail.com>
2019-10-04 14:50:26 +0000

    added cit groovy

--- cpl.groovy
+++ cpl.groovy
@@ -1,5 +1,27 @@
 folder("$BUName"){}
 folder("$BUName/$ProductName"){}
 pipelineJob("$BUName/$ProductName/CICD_$AppName"){
-
+	parameters{
+		stringParam("AppRepo", "$ApplicationRepo", "Git URL")
+		stringParam("NodeName", "$NodeName", "")
+		stringParam("AppName", "$AppName", "Application Name")
+		stringParam("UnitTestTool", "$UnitTestRun", "")
+		activeChoiceParam('Branch'){
+			filterable()
+			choiceType('SINGLE_SELECT')
+			groovyScript{
+				fallbackScript("Fallback choice script")	
+				script('["select", "master", "develop"]')
+			}
+			description("select the branch to be built.")
+		}
+	}
+	definition{
+		cps {
+			def jobScript = readFileFromWorkspace('cit.groovy')
+			def approvals = org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval.get()
+			approvals.approveScript(approvals.hash(jobScript, "groovy"))
+			script(jobScript)
+		}
+	}	
 }
